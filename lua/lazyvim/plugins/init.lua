eb1fae2ab9c14bec6d98aee4898a98aaa6a9b61a
wingedseraph <r89647@gmail.com>
2024-08-06 16:36:40 +0300

    config test

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -12,5 +12,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "wingedseraph/LazyVimx", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
+  -- { "wingedseraph/LazyVimx", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
 }
