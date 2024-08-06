f22fa507bdae2175edebed0099d74e0ca3e173be
wingedseraph <r89647@gmail.com>
2024-08-06 16:35:59 +0300

    f

--- lua/lazyvim/plugins/init.lua
+++ lua/lazyvim/plugins/init.lua
@@ -11,6 +11,6 @@ end
 require("lazyvim.config").init()
 
 return {
-  -- { "folke/lazy.nvim", version = "*" },
-  -- { "LazyVim/LazyVim", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
+  { "folke/lazy.nvim", version = "*" },
+  { "wingedseraph/LazyVimx", priority = 10000, lazy = false, opts = {}, cond = true, version = "*" },
 }
