c5280ce25e4acd8b5028ed98be7163f0c15b4489
wingedseraph <r89647@gmail.com>
2024-08-06 19:04:40 +0300

    foldexp

--- lua/lazyvim/util/ui.lua
+++ lua/lazyvim/util/ui.lua
@@ -101,7 +101,7 @@ function M.statuscolumn()
 
   local show_open_folds = vim.g.lazyvim_statuscolumn and vim.g.lazyvim_statuscolumn.folds_open
   local use_githl = vim.g.lazyvim_statuscolumn and vim.g.lazyvim_statuscolumn.folds_githl
-   local wanna = false
+  local wanna = false
   if show_signs and wanna then
     local signs = M.get_signs(buf, vim.v.lnum)
 
@@ -186,43 +186,43 @@ function M.color(name, bg)
   return color and string.format("#%06x", color) or nil
 end
 
-M.skip_foldexpr = {} ---@type table<number,boolean>
-local skip_check = assert(vim.uv.new_check())
-
-function M.foldexpr()
-  local buf = vim.api.nvim_get_current_buf()
-
-  -- still in the same tick and no parser
-  if M.skip_foldexpr[buf] then
-    return "0"
-  end
-
-  -- don't use treesitter folds for non-file buffers
-  if vim.bo[buf].buftype ~= "" then
-    return "0"
-  end
-
-  -- as long as we don't have a filetype, don't bother
-  -- checking if treesitter is available (it won't)
-  if vim.bo[buf].filetype == "" then
-    return "0"
-  end
-
-  local ok = pcall(vim.treesitter.get_parser, buf)
-
-  if ok then
-    return vim.treesitter.foldexpr()
-  end
-
-  -- no parser available, so mark it as skip
-  -- in the next tick, all skip marks will be reset
-  M.skip_foldexpr[buf] = true
-  skip_check:start(function()
-    M.skip_foldexpr = {}
-    skip_check:stop()
-  end)
-  return "0"
-end
+-- M.skip_foldexpr = {} ---@type table<number,boolean>
+-- local skip_check = assert(vim.uv.new_check())
+--
+-- function M.foldexpr()
+--   local buf = vim.api.nvim_get_current_buf()
+--
+--   -- still in the same tick and no parser
+--   if M.skip_foldexpr[buf] then
+--     return "0"
+--   end
+--
+--   -- don't use treesitter folds for non-file buffers
+--   if vim.bo[buf].buftype ~= "" then
+--     return "0"
+--   end
+--
+--   -- as long as we don't have a filetype, don't bother
+--   -- checking if treesitter is available (it won't)
+--   if vim.bo[buf].filetype == "" then
+--     return "0"
+--   end
+--
+--   local ok = pcall(vim.treesitter.get_parser, buf)
+--
+--   if ok then
+--     return vim.treesitter.foldexpr()
+--   end
+--
+--   -- no parser available, so mark it as skip
+--   -- in the next tick, all skip marks will be reset
+--   M.skip_foldexpr[buf] = true
+--   skip_check:start(function()
+--     M.skip_foldexpr = {}
+--     skip_check:stop()
+--   end)
+--   return "0"
+-- end
 
 ---@param buf number?
 function M.bufremove(buf)
