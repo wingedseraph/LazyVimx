a3a699cbf196aa76aef9cdb96eeda2abf3bc5596
wingedseraph <r89647@gmail.com>
2024-08-06 18:05:13 +0300

    buf remove mini

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -303,6 +303,36 @@ return {
     end,
   },
   {
+    "echasnovski/mini.bufremove",
+    keys = {
+      {
+        "<leader>x",
+        function()
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then -- Yes
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then -- No
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+        desc = "Delete Buffer",
+      },
+      {
+        "<leader>X",
+        function()
+          require("mini.bufremove").delete(0, true)
+        end,
+        desc = "Delete Buffer (Force)",
+      },
+    },
+  },
+  {
     import = "lazyvim.plugins.extras.editor.fzf",
     enabled = function()
       return LazyVimx.pick.want() == "fzf"
