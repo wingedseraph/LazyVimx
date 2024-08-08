62dd1978e18f0ec4ba2d7f27db826487fe4bd825
wingedseraph <r89647@gmail.com>
2024-08-08 20:47:44 +0300

    no mini hi patterns in deofualt

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -279,29 +279,7 @@ return {
       },
     },
   },
-  {
-    "echasnovski/mini.hipatterns",
-    version = "*",
-    event = "VeryLazy",
-    config = function()
-      -- vim.cmd.hi("clear Todo")
-      require("mini.hipatterns").setup()
-      local hipatterns = require("mini.hipatterns")
-      hipatterns.setup({
-        highlighters = {
-          -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE', 'DONE':
-          fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-          hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-          todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-          note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-          done = { pattern = "%f[%w]()DONE()%f[%W]", group = "Special" },
-
-          -- Highlight hex color strings (`#rrggbb`) using that color
-          hex_color = hipatterns.gen_highlighter.hex_color(),
-        },
-      })
-    end,
-  },
+  {},
   {
     "echasnovski/mini.bufremove",
     keys = {
